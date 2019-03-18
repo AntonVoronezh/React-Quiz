133d10fc04b2a8a7a3a5e13575539bc961077d5c
AntonVoronezh <pstut@mail.ru>
2019-03-18 22:02:14 +0300

    5#4

--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -1,16 +1,22 @@
 import React, { Component } from "react";
 import classes from "./Quiz.css";
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz.jsx";
 
 class Quiz extends Component {
-  state= {
+  state = {
     quiz: []
-  }
+  };
 
   render() {
-      return (
-         <div className={classes.Quiz}>
-         <h1>Quiz</h1></div>
-      )
+    return (
+      <div className={classes.Quiz}>
+       
+        <div className={classes.QiuzWrapper}>
+         <h1>Quiz</h1>
+          <ActiveQuiz />
+        </div>
+      </div>
+    );
   }
 }
 
