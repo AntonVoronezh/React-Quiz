971129cba7b9bcca38da4824955d492049d814be
AntonVoronezh <pstut@mail.ru>
2019-03-18 21:13:29 +0300

    4#1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import Layout from './hoc/Layout/Layout.jsx';
+import React, { Component } from "react";
+import Layout from "./hoc/Layout/Layout.jsx";
+import Quiz from "./containers/Quiz/Quiz.jsx";
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        
-        <div style={{width:400, border:'1px solid black'}}>
-        <h1>Layout</h1>
-        </div>
+        <Quiz />
       </Layout>
     );
   }
