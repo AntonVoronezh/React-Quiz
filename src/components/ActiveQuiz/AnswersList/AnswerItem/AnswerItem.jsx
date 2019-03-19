9d54c467d5ffdb3a10b7144fbfa4bc903588c6f9
AntonVoronezh <pstut@mail.ru>
2019-03-19 20:18:33 +0300

    9#5

--- src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.jsx
+++ src/components/ActiveQuiz/AnswersList/AnswerItem/AnswerItem.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import classes from "./AnswerItem.css";
 
 const AnswerItem = props => {
+  const cls = [classes.AnswerItem];
+
+  if (props.state) {
+    cls.push(classes[props.state]);
+  }
+
   return (
-    <li 
-    className={classes.AnswerItem}
-    onClick={() => props.onAnswerClick(props.answer.id)} 
+    <li
+      className={cls.join(' ')}
+      onClick={() => props.onAnswerClick(props.answer.id)}
     >
       {props.answer.text}
     </li>
