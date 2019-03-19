81b3f228a8d207a208a5c50a2fa255ee799557a7
AntonVoronezh <pstut@mail.ru>
2019-03-19 16:40:41 +0300

    6#1

--- src/components/ActiveQuiz/ActiveQuiz.jsx
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -1,22 +1,16 @@
 import React from "react";
 import classes from "./ActiveQuiz.css";
+import AnswersList from "./AnswersList/AnswersList.jsx";
 
 const ActiveQuiz = props => (
   <div className={classes.ActiveQuiz}>
     <p className={classes.Question}>
       <span>
-        <strong>2.</strong>&nbsp;
-        Как дела?
+        <strong>2.</strong>&nbsp; Как дела?
       </span>
       <small>4 из 12</small>
     </p>
-    <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-    </ul>
+    <AnswersList answers={props.answers} />
   </div>
 );
 
