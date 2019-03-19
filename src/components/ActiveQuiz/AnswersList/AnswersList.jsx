ea214e1293300a6aa0af1d0c42c012b4eb4b3ecb
AntonVoronezh <pstut@mail.ru>
2019-03-19 17:18:03 +0300

    7#4

--- src/components/ActiveQuiz/AnswersList/AnswersList.jsx
+++ src/components/ActiveQuiz/AnswersList/AnswersList.jsx
@@ -5,7 +5,13 @@ import AnswerItem from "./AnswerItem/AnswerItem.jsx";
 const AnswersList = props => (
   <ul className={classes.AnswersList}>
     {props.answers.map((answer, index) => {
-      return <AnswerItem answer={answer} key={index} />;
+      return (
+        <AnswerItem
+          answer={answer}
+          key={index}
+          onAnswerClick={props.onAnswerClick}
+        />
+      );
     })}
   </ul>
 );
