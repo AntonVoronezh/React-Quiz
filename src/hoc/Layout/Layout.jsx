529283f3204ac3400ea05aacf09639dd6e38afa1
AntonVoronezh <pstut@mail.ru>
2019-03-20 20:34:45 +0300

    14#1

--- src/hoc/Layout/Layout.jsx
+++ src/hoc/Layout/Layout.jsx
@@ -1,19 +1,30 @@
-import React, { Component } from 'react';
-import classes from './Layout.css';
-
+import React, { Component } from "react";
+import classes from "./Layout.css";
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle.jsx";
 
 class Layout extends Component {
-    render() {
-      return (
-        <div className={classes.Layout}>
-          
-          <main>
-              {this.props.children}
-          </main>
-        </div>
-      )
-    }
+  state = {
+    menu: false
+  };
+
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu
+    });
+  };
+
+  render() {
+    return (
+      <div className={classes.Layout}>
+        <MenuToggle 
+        onToggle={this.toggleMenuHandler} 
+        isOpen={this.state.menu}
+        />
+
+        <main>{this.props.children}</main>
+      </div>
+    );
   }
-  
-  export default Layout;
-  
+}
+
+export default Layout;
