144cd7f1174f13511431f10e5b7243de4b297700
AntonVoronezh <pstut@mail.ru>
2019-03-20 15:37:18 +0300

    12#2

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -2,23 +2,36 @@ import React from "react";
 import classes from "./FinishedQuiz.css";
 
 const FinishedQuiz = props => {
+
+    const successCount = Object.keys(props.results).reduce((total, key)=>{
+        if(props.results[key] === 'success') {
+            total++;
+        }
+        return total;
+    },0);
+
   return (
     <div className={classes.FinishedQuiz}>
       <ul>
-        <li>
-          <strong>1.</strong>
-          gdfgdhdfh
-          <i className={"fa fa-times " + classes.error} />
-        </li>
-        <li>
-          <strong>1.</strong>
-          gdfgdhdfh
-          <i className={"fa fa-check " + classes.success} />
-        </li>
+        {props.quiz.map((quizItem, index) => {
+          const cls = [
+            "fa",
+            props.results[quizItem.id] === "error" ? "fa-times" : "fa-check",
+            classes[props.results[quizItem.id]]
+          ];
+
+          return (
+            <li key={index}>
+              <strong>{index + 1}</strong>. &nbsp;
+              {quizItem.question}
+              <i className={cls.join(" ")} />
+            </li>
+          );
+        })}
       </ul>
-      <p>Правильно 4 из 10</p>
+      <p>Правильно {successCount} из {props.quiz.length}</p>
       <div>
-        <button> повторить</button>
+        <button onClick={props.onRetry}> повторить</button>
       </div>
     </div>
   );
