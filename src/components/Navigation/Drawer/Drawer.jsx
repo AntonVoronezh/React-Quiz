9b1b5e3f7d3361aa8872fd4ca72345eba1bc4d10
AntonVoronezh <pstut@mail.ru>
2019-03-20 21:26:30 +0300

    16#1

--- src/components/Navigation/Drawer/Drawer.jsx
+++ src/components/Navigation/Drawer/Drawer.jsx
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
 import classes from "./Drawer.css";
+import Backdrop from "../../UI/Backdrop/Backdrop.jsx";
 
 const links = [1, 2, 3];
 
 class Drawer extends Component {
   renderLinks() {
     return links.map((link, index) => {
-      return <li key={index}
-      ><a>link {link}</a>
-      </li>;
-      
+      return (
+        <li key={index}>
+          <a>link {link}</a>
+        </li>
+      );
     });
   }
 
   render() {
     const cls = [classes.Drawer];
 
-    if(!this.props.isOpen) {
-        cls.push(classes.close)
+    if (!this.props.isOpen) {
+      cls.push(classes.close);
     }
 
     return (
-      <nav className={cls.join(' ')}>
-        <ul>{this.renderLinks()}</ul>
-      </nav>
+      <React.Fragment>
+        <nav className={cls.join(" ")}>
+          <ul>{this.renderLinks()}</ul>
+        </nav>
+        {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+      </React.Fragment>
     );
   }
 }
