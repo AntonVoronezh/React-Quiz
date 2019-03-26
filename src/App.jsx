3b9e6cdcc45eb70cc660ce7f47bc0d32c75c6f54
AntonVoronezh <pstut@mail.ru>
2019-03-26 18:07:23 +0300

    2#4

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import Layout from "./hoc/Layout/Layout.jsx";
 import Quiz from "./containers/Quiz/Quiz.jsx";
+import Auth from "./containers/Auth/Auth.jsx";
+import QuizCreator from "./containers/QuizCreator/QuizCreator.jsx";
+import QuizList from "./containers/QuizList/QuizList.jsx";
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/quiz-creator" component={QuizCreator} />
+          <Route path="/quiz/:id" component={Quiz} />
+          <Route path="/" component={QuizList} />
+        </Switch>
       </Layout>
     );
   }
