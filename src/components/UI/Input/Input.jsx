6a5cf543435238eaafe2e524156348e5b63f62ef
AntonVoronezh <pstut@mail.ru>
2019-03-29 19:05:57 +0300

    8

--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import classes from "./Input.css";
 
+function isInvalid({valid, touched, shouldValidate}) {
+    return !valid && touched && shouldValidate;
+}
 
 const Input = props => {
 
