c50961b7a512e24ca77e85823769890a90e8bc67
AntonVoronezh <pstut@mail.ru>
2019-03-29 19:05:41 +0300

    6

---
+++ src/components/UI/Input/Input.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import classes from "./Input.css";
+
+
+const Input = props => {
+
+
+  return (
+    <div className={cls.join(" ")}>
+    </div>
+  );
+};
+
+export default Input;
