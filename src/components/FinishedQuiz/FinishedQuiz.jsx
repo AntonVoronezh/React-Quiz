6e1f8606a2535346668fa19a382232f72412b549
AntonVoronezh <pstut@mail.ru>
2019-03-29 19:04:04 +0300

    3

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "./FinishedQuiz.css";
 import Button from "../UI/Button/Button.jsx";
 
 const FinishedQuiz = props => {
-
-    const successCount = Object.keys(props.results).reduce((total, key)=>{
-        if(props.results[key] === 'success') {
-            total++;
-        }
-        return total;
-    },0);
+  const successCount = Object.keys(props.results).reduce((total, key) => {
+    if (props.results[key] === "success") {
+      total++;
+    }
+    return total;
+  }, 0);
 
   return (
     <div className={classes.FinishedQuiz}>
@@ -30,16 +30,16 @@ const FinishedQuiz = props => {
           );
         })}
       </ul>
-      <p>Правильно {successCount} из {props.quiz.length}</p>
+      <p>
+        Правильно {successCount} из {props.quiz.length}
+      </p>
       <div>
-        <Button 
-          onClick={props.onRetry}
-          type="primary"
-          
-          >повторить</Button>
-        <Button 
-          type="success"
-          >перейти в список тестов</Button>
+        <Button onClick={props.onRetry} type="primary">
+          повторить
+        </Button>
+        <Link to='/'>
+          <Button type="success">перейти в список тестов</Button>
+        </Link>
       </div>
     </div>
   );
