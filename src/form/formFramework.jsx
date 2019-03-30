5e2ba7c7896a9ebfff443115397277a75b50c132
AntonVoronezh <pstut@mail.ru>
2019-03-30 17:09:42 +0300

    17

--- src/form/formFramework.jsx
+++ src/form/formFramework.jsx
@@ -25,7 +25,11 @@ export function validate(value, validation = null){
 export function validateForm(formControls) {
     let isFormValid = true
 
-
+    for(let control in formControls) {
+        if(formControls.hasOwnProperty(control)) {
+            isFormValid = formControls[control].valid && isFormValid
+        }
+    }
 
     return isFormValid
 }
