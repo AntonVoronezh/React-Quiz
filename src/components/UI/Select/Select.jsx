405415f9a9eb0575a750b39aba4f4cf837d959cb
AntonVoronezh <pstut@mail.ru>
2019-03-30 17:07:25 +0300

    11

--- src/components/UI/Select/Select.jsx
+++ src/components/UI/Select/Select.jsx
@@ -11,7 +11,9 @@ const Select = props => {
       <select id={htmlFor} value={props.value} onChange={props.onChange}>
         {props.options.map((option, index) => {
           return (
-
+            <option value={option.value} key={option.value + index}>
+              {option.text}
+            </option>
           );
         })}
 
