94eedfe1b85df5c965242d9d492d0d7174911194
AntonVoronezh <pstut@mail.ru>
2019-03-30 17:06:21 +0300

    6

---
+++ src/components/UI/Select/Select.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./Select.css";
+
+
+const Select = props => {
+
+
+  return (
+    <div className={classes.Select}>
+
+      </select>
+    </div>
+  );
+};
+
+export default Select;
