748feded0cb05261da91beef6959399ebd04064b
AntonVoronezh <pstut@mail.ru>
2019-04-03 17:06:58 +0300

    3#2

--- src/store/actions/quiz.js
+++ src/store/actions/quiz.js
@@ -1,5 +1,10 @@
 import axios from '../../axios/axios-quiz';
-import {FETCH_QUIZES_ERROR, FETCH_QUIZES_START, FETCH_QUIZES_SUCCESS} from './actionTypes'
+import {
+    FETCH_QUIZES_ERROR,
+    FETCH_QUIZES_START,
+    FETCH_QUIZES_SUCCESS,
+    FETCH_QUIZ_SUCCESS
+} from './actionTypes'
 
 export function fetchQuizes() {
     return async dispatch => {
@@ -18,30 +23,55 @@ export function fetchQuizes() {
             });
 
             dispatch(fetchQuizesSuccess(quizes));
-           
+
         } catch (err) {
             dispatch(fetchQuizesError(err));
         }
-        
+
     }
 }
 
-export function fetchQuizesStart(){
+export function fetchQuizesStart() {
     return {
         type: FETCH_QUIZES_START
     }
 }
 
-export function fetchQuizesSuccess(quizes){
+export function fetchQuizesSuccess(quizes) {
     return {
         type: FETCH_QUIZES_SUCCESS,
         quizes
     }
 }
 
-export function fetchQuizesError(err){
+export function fetchQuizesError(err) {
     return {
         type: FETCH_QUIZES_ERROR,
         errror: err
+    }
+}
+
+export function fetchQuizById(quizId) {
+    return async dispatch => {
+        dispatch(fetchQuizesStart());
+
+        try {
+            const response = await axios.get(
+                `quizes/${quizId}.json`
+            );
+            const quiz = response.data;
+
+            dispatch(fetchQuizSuccess(quiz));
+
+        } catch (err) {
+            dispatch(fetchQuizesError(err));
+        }
+    }
+}
+
+export function fetchQuizSuccess(quiz) {
+    return {
+        type: FETCH_QUIZ_SUCCESS,
+        quiz
     }
 }
