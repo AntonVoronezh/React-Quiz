f8bea5a95975b19b2dcebbc8ccd44507dff9968f
AntonVoronezh <pstut@mail.ru>
2019-04-04 17:29:53 +0300

    1

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,52 @@ import Quiz from "./containers/Quiz/Quiz.jsx";
 import Auth from "./containers/Auth/Auth.jsx";
 import QuizCreator from "./containers/QuizCreator/QuizCreator.jsx";
 import QuizList from "./containers/QuizList/QuizList.jsx";
-import {Route, Switch} from 'react-router-dom';
+import Logout from "./components/Logout/Logout.jsx";
+import { Route, Switch, Redirect, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import {autoLogin} from './store/actions/auth'
 
 class App extends Component {
+  componentDidMount() {
+    this.props.autoLogin();
+  }
+
   render() {
-    return (
-      <Layout>
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/" exact component={QuizList} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
         <Switch>
-          <Route path="/auth" component={Auth} />
           <Route path="/quiz-creator" component={QuizCreator} />
           <Route path="/quiz/:id" component={Quiz} />
-          <Route path="/" component={QuizList} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={QuizList} />
+          <Redirect to="/" />
         </Switch>
-      </Layout>
-    );
+      );
+    }
+
+    return <Layout>{routes}</Layout>;
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.auth.token
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    autoLogin: () => dispatch(autoLogin())
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
