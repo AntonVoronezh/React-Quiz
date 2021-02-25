97a756824f9dac0180b1622bb01c2e7d6af14256
Anton <pstut@mail.ru>
2021-02-25 17:33:46 +0300

    add types

---
+++ src/containers/QuizList/QuizList.tsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+import {connect} from "react-redux";
+
+import classes from "./QuizList.css";
+import { fetchQuizes } from "../../store/actions/quiz";
+import {Loader} from "../../components/UI/Loader/Loader";
+
+class QuizList extends Component {
+  renderQuizes() {
+    return this.props.quizes.map(quiz => {
+      return (
+        <li key={quiz.id}>
+          <NavLink to={"/quiz/" + quiz.id}> {quiz.name}</NavLink>
+        </li>
+      );
+    });
+  }
+
+  componentDidMount() {
+    this.props.fetchQuizes();
+  }
+
+  render() {
+    return (
+      <div className={classes.QuizList}>
+        <div>
+          <h1>Список тестов</h1>
+          {this.props.loading && this.props.quizes !== 0 ? (
+            <Loader />
+          ) : (
+            <ul>{this.renderQuizes()}</ul>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    quizes: state.quiz.quizes,
+    loading: state.quiz.loading
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchQuizes: () => dispatch(fetchQuizes())
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(QuizList);
