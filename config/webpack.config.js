8c90a9ce6f47b8b2cf5aea46c57a0df63cb9e2ea
Anton <pstut@mail.ru>
2021-02-25 17:33:57 +0300

    add types

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -299,7 +299,7 @@ module.exports = function (webpackEnv) {
         // First, run the linter.
         // It's important to do this before Babel processes the JS.
         {
-          test: /\.(js|mjs|jsx)$/,
+          test: /\.(js|mjs|jsx|ts|tsx)$/,
           enforce: 'pre',
           use: [{
             options: {
